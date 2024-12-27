1a22558e35fd873d9b723dda4476b80a6ea651dd
rj <rj@oxyguard.dk>
2024-12-27 13:32:13 +0100

    Init

---
+++ apps/my-workspace/rspack.config.js
@@ -0,0 +1,11 @@
+const { composePlugins, withNx } = require('@nx/rspack');
+
+module.exports = composePlugins(withNx(), (config) => {
+    return {
+        ...config,
+        ignoreWarnings: [
+            // Disable non-critical warnings for a prettier output:
+            /HarmonyLinkingWarning/, warning => false,
+        ],
+    };
+});
